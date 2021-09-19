d50c9e44c6f0c8758242bcc606413eb08d0e25f5
Jon Deavers <jondeavers@gmail.com>
2021-09-19 12:32:24 -0400

    Portfolio init

--- pages/portfolio.js
+++ pages/portfolio.js
@@ -1,11 +1,32 @@
+import { useState } from 'react';
 import Layout from '../components/Layout';
+import { Container, Row, Col } from 'react-bootstrap';
 
-const Portfolio = () => {
+export default function Portfolio() {
+  const [state, setState] = useState([
+    {
+      title: 'Sample item',
+      description: 'This is a sample item for my portfolio page',
+      background: 'This is a sample background info for my portfolio page',
+    },
+  ]);
+
   return (
     <Layout>
-      <h1>Portfolio Under Construction</h1>
+      <Container>
+        <Row>
+          <Col>
+            <h1 className='text-center'>Portfolio</h1>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <h2>{state[0].title}</h2>
+            <p>{state[0].description}</p>
+            <p>{state[0].background}</p>
+          </Col>
+        </Row>
+      </Container>
     </Layout>
   );
-};
-
-export default Portfolio;
+}
